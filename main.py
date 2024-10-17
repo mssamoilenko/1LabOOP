00cc817d2b4ce999cf7a24c3b1b180a8a7cfd713
Alla <allasamoilenko@gmx.de>
2024-10-18 00:27:26 +0300

    task2

--- main.py
+++ main.py
@@ -25,3 +25,28 @@ Person1 = Person("Самойленко Настя Ігорівна","28.10.1998"
 print(Person1.getInfo())
 Person1.update_phone("+380960960179")
 print(Person1.getInfo())
+
+#task2
+class City:
+    def __init__(self, name, region, country, population, postal_code, phone_code):
+        self.name = name
+        self.region = region
+        self.country = country
+        self.population = population
+        self.postal_code = postal_code
+        self.phone_code = phone_code
+
+    def get_info(self):
+        return (f"\nНазва міста: {self.name}, \nРегіон: {self.region}, "
+                f"\nКраїна: {self.country}, \nКількість жителів: {self.population}, "
+                f"\nПоштовий індекс: {self.postal_code}, \nТелефонний код: {self.phone_code}")
+
+    def update_population(self, new_population):
+        self.population = new_population
+        print(f"\nКількість жителів оновлена: {self.population}")
+
+city1 = City("Київ", "Київська область", "Україна", 2884000, "01001", "+38044")
+print(city1.get_info())
+city1.update_population(2900000)
+print(city1.get_info())
+
